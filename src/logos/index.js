62022485cddf5d97dacea4687cef6bc5686d4f7c
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-11 19:01:13 +0100

    adding be logos

---
+++ src/logos/index.js
@@ -0,0 +1,7 @@
+import BeeLogoLargeBordered  from '../logos/BeeLogoLargeBordered.png';
+import BeeLogoLarge from '../logos/BeeLogoLarge.png';
+import BeeLogo500px from '../logos/BeeLogo500px.png';
+import BeeLogo250px from '../logos/BeeLogo250px.png';
+import BeeLogo100px from '../logos/BeeLogo100px.png';
+
+export {BeeLogoLargeBordered, BeeLogoLarge, BeeLogo500px, BeeLogo250px, BeeLogo100px}
