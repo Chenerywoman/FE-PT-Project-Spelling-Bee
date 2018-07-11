9642303754827423ecdc6b311fa05b710c0ece74
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-11 20:54:08 +0100

    initial render

---
+++ src/components/ContentBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import WordBox from './WordBox'
+
+const ContentBox = (words) => {
+return (
+    <div> <WordBox /></div>
+)
+}
+
+export default ContentBox;
