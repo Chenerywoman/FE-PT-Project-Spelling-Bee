76575338d3914f82f793aec34bbb690a3c76e2e9
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-11 19:39:18 +0100

    all routes using CategoriesPage working

--- src/components/App.js
+++ src/components/App.js
@@ -12,13 +12,13 @@ class App extends Component {
         <Switch>
           <Route exact path = '/' render={props => (<HomePage />)}/>
           <Route exact path = '/prefixes' render={props => (<CategoriesPage />)}/>
-          {/* <Route exact path = '/prefixes/:prefix' render={props => (<PracticePage />)}/> */}
+          <Route exact path = '/prefixes/:prefix' render={props => (<PracticePage />)}/>
           <Route exact path = '/suffixes' render={props => (<CategoriesPage />)}/>
-          {/* <Route exact path = '/suffixes/:suffix' render={props => (<PracticePage />)}/> */}
+          <Route exact path = '/suffixes/:suffix' render={props => (<PracticePage />)}/>
           <Route exact path = '/medials' render={props => (<CategoriesPage />)}/>
-          {/* <Route exact path = '/medials/:medial' render={props => (<PracticePage />)}/> */}
+          <Route exact path = '/medials/:medial' render={props => (<PracticePage />)}/>
           <Route exact path = '/homophones' render={props => (<CategoriesPage />)}/>
-          {/* <Route exact path = '/homophones/:homophone' render={props => (<PracticePage />)}/> */}
+          <Route exact path = '/homophones/:homophone' render={props => (<PracticePage />)}/>
           <Route exact path = '/freestyle' render={props => (<CategoriesPage />)}/>
           <Route exact path = '/allwords' render={props => (<CategoriesPage />)}/>  
           <Route component={<NoMatchPage/>}/>
