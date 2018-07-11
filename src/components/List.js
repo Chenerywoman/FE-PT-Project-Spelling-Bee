da799543bbc1c0cc5c89709756d332bd97fe47df
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-11 20:54:26 +0100

    initial render

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import WordBox from './WordBox';
+
+const List = (words) => {
+    return (
+
+      <p>list of words</p>   
+    )
+
+}
+
+export default List;
