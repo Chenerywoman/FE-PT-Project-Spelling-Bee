1839623b39dfa5132103f69a878ca1afc1996054
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-11 19:26:32 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import HomePage from './HomePage';
+import CategoriesPage from './CategoriesPage';
+import PracticePage from'./PracticePage';
+import NoMatchPage from './NoMatchPage';
+
+export {HomePage, CategoriesPage, PracticePage, NoMatchPage}
