d69eeb96ba7d4621ae4475f3e8390751cb670599
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-11 20:56:06 +0100

    adding ContentBox & List to render

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Link from 'react-router-dom';
 import {BeeLogo100px, BeeLogo250px, BeeLogo500px, BeeLogoLarge, BeeLogoLargeBordered} from '../logos';
-import '../styling/pages/HomePage.css'
+import {ContentBox, List} from '../components';
+import '../styling/pages/HomePage.css';
 
 const HomePage = props => {
     return (
       <React.Fragment>
-    <h1>Welcome to Spelling Bee!</h1>
         <img src={BeeLogoLarge} id="BeeLogoLarge" className="bee-logo" alt="BeeLogoLarge" />
+        <div className="wrapper">
+        <header className="main-head"><h1>Welcome to Spelling Bee!</h1></header>
+        <ContentBox className="content"/>
+        <List className="list"/>
+
+        
+
+
+
+
+        </div>
    </React.Fragment>
     );
 
