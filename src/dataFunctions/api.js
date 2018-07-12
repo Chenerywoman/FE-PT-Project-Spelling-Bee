518d0e499ddddab234fca5d8f6ef388ff8e3f160
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-12 14:46:48 +0100

    adding function getCategory

---
+++ src/dataFunctions/api.js
@@ -0,0 +1,14 @@
+const API_URL = 'http://localhost:3000/api';
+
+exports.getCategory = (category) => {
+category = category.toLowerCase()
+    const url = `${API_URL}/${category}`
+    return fetch(url)
+    .then(res => {
+        console.log('res after fetch', res)
+        // or should this be res.statusText????
+        if (res.status === 404) throw {status: res.status}
+        else return res.json()
+    })
+    .then(res => res[category])
+}
