2cb0eafe2cbaf277c221b0aa01c453c17e389897
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-12 15:48:47 +0100

    rendering category letters as buttons

--- src/components/WordBox.js
+++ src/components/WordBox.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const WordBox = (word) => {
+const WordBox = ({letter}) => {
+    console.log('letter in wordbox', letter)
 return (
-     <p>WordBox</p>
+    <button>{letter}</button>
 )
 }
 
