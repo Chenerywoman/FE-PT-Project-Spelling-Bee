7e6bfe7b2cf93019bc39555d68e67dbb5f47376b
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-12 15:50:21 +0100

    rendering category letters in WordBox

--- src/components/List.js
+++ src/components/List.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 
 import WordBox from './WordBox';
 
-const List = (words) => {
-    return (
+const List = ({ letters, category }) => {
 
-      <p>list of words</p>   
-    )
-
+  return (
+    <div>
+      {letters.map(letter => <WordBox key={letter} letter={letter} />)}
+    </div>
+  )
 }
 
 export default List;
