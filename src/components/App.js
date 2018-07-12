7b65cc1c19d09cd914b541243bf0b358055ad96f
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-12 15:03:50 +0100

    passing category as props to each route

--- src/components/App.js
+++ src/components/App.js
@@ -11,16 +11,16 @@ class App extends Component {
       <React.Fragment>
         <Switch>
           <Route exact path = '/' render={props => (<HomePage />)}/>
-          <Route exact path = '/prefixes' render={props => (<CategoriesPage />)}/>
-          <Route exact path = '/prefixes/:prefix' render={props => (<PracticePage />)}/>
-          <Route exact path = '/suffixes' render={props => (<CategoriesPage />)}/>
-          <Route exact path = '/suffixes/:suffix' render={props => (<PracticePage />)}/>
-          <Route exact path = '/medials' render={props => (<CategoriesPage />)}/>
-          <Route exact path = '/medials/:medial' render={props => (<PracticePage />)}/>
-          <Route exact path = '/homophones' render={props => (<CategoriesPage />)}/>
-          <Route exact path = '/homophones/:homophone' render={props => (<PracticePage />)}/>
-          <Route exact path = '/freestyle' render={props => (<CategoriesPage />)}/>
-          <Route exact path = '/allwords' render={props => (<CategoriesPage />)}/>  
+          <Route exact path = '/prefixes' render={props => (<CategoriesPage category='Prefixes'/>)}/>
+          <Route exact path = '/prefixes/:prefix' render={props => (<PracticePage category='Prefixes'/>)}/>
+          <Route exact path = '/suffixes' render={props => (<CategoriesPage category='Suffixes'/>)}/>
+          <Route exact path = '/suffixes/:suffix' render={props => (<PracticePage category='Suffixes'/>)}/>
+          <Route exact path = '/medials' render={props => (<CategoriesPage category='Medials'/>)}/>
+          <Route exact path = '/medials/:medial' render={props => (<PracticePage category='Medials'/>)}/>
+          <Route exact path = '/homophones' render={props => (<CategoriesPage category='Homophones'/>)}/>
+          <Route exact path = '/homophones/:homophone' render={props => (<PracticePage category='Homophones'/>)}/>
+          <Route exact path = '/freestyle' render={props => (<CategoriesPage category='Freestyle'/>)}/>
+          <Route exact path = '/allwords' render={props => (<CategoriesPage category='Allwords'/>)}/>  
           <Route component={NoMatchPage}/>
       </Switch>
       </React.Fragment>
