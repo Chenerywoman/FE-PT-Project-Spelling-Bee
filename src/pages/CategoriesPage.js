b2441d92aeac18af2e0b63eea7629874dc93a0f1
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 21:37:32 +0100

    adding getList to fetchCategory function

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,47 +1,41 @@
 import React, {Component} from 'react';
-import Link from 'react-router-dom';
+// import Link from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { BeeLogoLarge} from '../logos';
 import {ContentBox, List} from '../components';
 import '../styling/pages/CategoriesPage.css';
-import {getCategory} from '../dataFunctions/api'
+import {getCategory, getList} from '../dataFunctions/api'
 
 class CategoriesPage extends Component {
 
     state = {
-
-        category: '',
-        description: '',
-        letters: [], 
+        category: {},
+        list: [],
+        year: this.props.match.params.year,
         loading: true
     }
 
     fetchCategory = (category) => {
         return getCategory(category)
         .then(category => {
-            this.setState({
-                category: category.category,
-                description: category.description,
-                letters: category.letters, 
-                loading: false
-            })
+            this.setState({category})
+            return getList(this.state.category.name)
         })
-        .catch(() => {
-            this.props.history.push('/404');
-        });
+        .then(res => this.setState({list: res[this.state.category.name], loading: false}))
+        .catch(err => this.props.history.push('/404'))
     }
 
     componentDidMount() {
         console.log('in component did mount')
-        const category = this.props.category
+        const category = this.props.category;
         this.fetchCategory(category)
     }
 
-    componentDidUpdate(prevProps) {
+    componentDidUpdate(prevProps, prevState) {
         console.log('in component did update')
         const currCategory = this.props.category
         const prevCategory = prevProps.category
-
+// add for change in year in params 
         if (currCategory !== prevCategory) {
             this.setState({loading: true})
             this.fetchCategory(currCategory)
@@ -54,10 +48,10 @@ class CategoriesPage extends Component {
           {this.state.loading ? <p>loading...</p>  
           :
           <React.Fragment>
-    <h1>{this.props.category}</h1>
+    <h1>Year {this.state.year} {this.props.category}</h1>
         <img src={BeeLogoLarge} id="BeeLogoLarge" className="bee-logo" alt="BeeLogoLarge" />
-        <ContentBox className="content" description={this.state.description}/>
-        <List className="list" letters={this.state.letters} category={this.state.category}/>
+        <ContentBox className="content" description={this.state.category.description}/>
+        <List className="list" items={this.state.list} year={this.state.year} page='categories'/>
         </React.Fragment>
     }
    </React.Fragment>
