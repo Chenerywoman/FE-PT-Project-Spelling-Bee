9a3f7c137a1bb2a8419adc8dd6facd9213b7a5b5
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 21:39:47 +0100

    adding additional part to ternary for categories page

--- src/components/WordBox.js
+++ src/components/WordBox.js
@@ -3,14 +3,15 @@ import {Link} from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
 const WordBox = ({item, page, year}) => {
-    console.log('item', item )
+    console.log('item', item)
 return (
     <React.Fragment>
-    { page === 'home' ? <Link to={`/year${item.year}`}><button>{item.year}</button></Link> :
-
+    { page === 'home' ? <Link to={`/year${item.year}`}><button>{item.year}</button></Link> 
+    :
     page === 'year' ? <Link to={`/year${year}/${item.name}`}><button>{item.name}</button></Link>
    : 
-   <div></div>
+   page === 'categories' ? <Link to={`/year${year}/${item.categories[0].name}/${item.letters}`}><button>{item.letters}</button></Link>
+   : <div></div>
     }
     </React.Fragment>
 )
