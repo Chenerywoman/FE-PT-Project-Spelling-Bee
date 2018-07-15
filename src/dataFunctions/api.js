2ac3cae649e7b4ab4dd90f77b5200af31bc4e3f2
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 14:43:42 +0100

    adding findYears function

--- src/dataFunctions/api.js
+++ src/dataFunctions/api.js
@@ -1,5 +1,16 @@
 const API_URL = 'http://localhost:3000/api';
 
+exports.findYears = () => {
+
+    const url = `${API_URL}/years`
+    return fetch(url)
+    .then(res => {
+        if (res.status === 404) throw new Error(res.statusText)
+        else return res.json()
+    })
+    .then(res =>  res)
+}
+
 exports.getCategory = (category) => {
 category = category.toLowerCase()
     const url = `${API_URL}/${category}`
