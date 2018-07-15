2ebbe0cf1ddcac7d29812b50a5bff83c844280fc
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 21:42:31 +0100

    refactoring getCategory function & adding getList

--- src/dataFunctions/api.js
+++ src/dataFunctions/api.js
@@ -22,11 +22,24 @@ exports.findCategoriesByYear  = (year) => {
 
 exports.getCategory = (category) => {
 category = category.toLowerCase()
+    const url = `${API_URL}/categories/${category}`
+    return fetch(url)
+    .then(res => {
+        if (res.status === 404) throw new Error(res.statusText)
+        else return res.json()
+    })
+    .then(res => res.category[0])
+}
+
+exports.getList = (category) => {
+    category = category.toLowerCase()
     const url = `${API_URL}/${category}`
     return fetch(url)
     .then(res => {
         if (res.status === 404) throw new Error(res.statusText)
         else return res.json()
     })
-    .then(res => res[category][0])
+    .then(res => res)
+
+
 }
