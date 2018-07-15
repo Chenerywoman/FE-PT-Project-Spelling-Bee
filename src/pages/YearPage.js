ae965a83b05bcd0f5a74dcf07121a317e0ab51dd
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 17:57:32 +0100

    adding conditional rendering of categories if exist for page or holding message if not

--- src/pages/YearPage.js
+++ src/pages/YearPage.js
@@ -3,17 +3,21 @@ import {Redirect} from 'react-router-dom';
 import {BeeLogoLarge} from '../logos';
 import {ContentBox, List} from '../components';
 import '../styling/pages/YearPage.css';
+import { findCategoriesByYear } from '../dataFunctions/api';
 
 class YearPage extends Component {
 
     state = {
-        year: '',
-        // year?
+        categories:[],
         loading: true
     }
 
 componentDidMount () {
-
+    return findCategoriesByYear(this.props.match.params.year)
+    .then(({categories}) =>  this.setState({categories, loading: false}))
+    .catch(error => {
+        this.props.history.push('/404');
+      });
 }
 
 componentDidUpdate () {
@@ -22,7 +26,7 @@ componentDidUpdate () {
 }
 
 render(){
-    console.log('this.props.match.params.year', this.props.match.params.year)
+    console.log('this.state.categories', this.state.categories)
 return (
     <React.Fragment>
     { !/^[1-6]$/.test(this.props.match.params.year) ? < Redirect to='404' /> 
@@ -30,8 +34,13 @@ return (
     <React.Fragment>
     <header><p>Year {this.props.match.params.year} page</p></header>
     <img src={BeeLogoLarge} id="BeeLogoLarge" className="bee-logo" alt="BeeLogoLarge" />
+    { !this.state.categories.length ? <p>We do not currently have data for year {`${this.props.match.params.year}`}</p>
+    :
+    <React.Fragment>
     <ContentBox/>
-    {/* <List /> */}
+    <List items={this.state.categories} year={this.props.match.params.year} page='year'/> 
+    </React.Fragment>
+    }
     </React.Fragment>
     }
     </React.Fragment>
