126cc1820c2beef80cd9067c0a998eaf5c091e36
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 15:11:43 +0100

    removing import of NoMatchPage

--- src/pages/YearPage.js
+++ src/pages/YearPage.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import {Redirect} from 'react-router-dom';
 import {BeeLogoLarge} from '../logos';
-import {ContenBox, List} from '../components';
+import {ContentBox, List} from '../components';
 import '../styling/pages/YearPage.css';
-import { NoMatchPage } from '.';
 
 class YearPage extends Component {
 
@@ -26,11 +25,13 @@ render(){
     console.log('this.props.match.params.year', this.props.match.params.year)
 return (
     <React.Fragment>
-    { !/^[1-6]$/.test(this.props.match.params.year) ? < NoMatchPage /> 
+    { !/^[1-6]$/.test(this.props.match.params.year) ? < Redirect to='404' /> 
     :
     <React.Fragment>
     <header><p>Year {this.props.match.params.year} page</p></header>
     <img src={BeeLogoLarge} id="BeeLogoLarge" className="bee-logo" alt="BeeLogoLarge" />
+    <ContentBox/>
+    {/* <List /> */}
     </React.Fragment>
     }
     </React.Fragment>
