73e9c2665def32c28480dfe27cb3bc4d9d2deeac
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 14:51:45 +0100

    adding import & export of YearPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import HomePage from './HomePage';
+import YearPage from './YearPage';
 import CategoriesPage from './CategoriesPage';
 import PracticePage from'./PracticePage';
 import NoMatchPage from './NoMatchPage';
 
-export {HomePage, CategoriesPage, PracticePage, NoMatchPage}
+export {HomePage, YearPage, CategoriesPage, PracticePage, NoMatchPage}
