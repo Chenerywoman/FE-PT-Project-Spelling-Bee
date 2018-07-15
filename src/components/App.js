820621f5cc5cf8540f9293eb3ecd7235a8cd9d38
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 14:40:55 +0100

    adding word 'year' followed by path parameter - year to each route

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import '../styling/components/App.css';
 
-import {HomePage, CategoriesPage, PracticePage, NoMatchPage} from '../pages';
+import {HomePage, YearPage, CategoriesPage, PracticePage, NoMatchPage} from '../pages';
 
 class App extends Component {
   render() {
@@ -11,16 +11,17 @@ class App extends Component {
       <React.Fragment>
         <Switch>
           <Route exact path = '/' render={props => (<HomePage />)}/>
-          <Route exact path = '/prefixes' render={props => (<CategoriesPage category='Prefixes'/>)}/>
-          <Route exact path = '/prefixes/:prefix' render={props => (<PracticePage category='Prefixes'/>)}/>
-          <Route exact path = '/suffixes' render={props => (<CategoriesPage category='Suffixes'/>)}/>
-          <Route exact path = '/suffixes/:suffix' render={props => (<PracticePage category='Suffixes'/>)}/>
-          <Route exact path = '/medials' render={props => (<CategoriesPage category='Medials'/>)}/>
-          <Route exact path = '/medials/:medial' render={props => (<PracticePage category='Medials'/>)}/>
-          <Route exact path = '/homophones' render={props => (<CategoriesPage category='Homophones'/>)}/>
-          <Route exact path = '/homophones/:homophone' render={props => (<PracticePage category='Homophones'/>)}/>
-          <Route exact path = '/freestyle' render={props => (<CategoriesPage category='Freestyle'/>)}/>
-          <Route exact path = '/allwords' render={props => (<CategoriesPage category='Allwords'/>)}/>  
+          <Route exact path = '/year:year' render={props => (<YearPage {...props}/>)}/>
+          <Route exact path = '/year:year/prefixes' render={props => (<CategoriesPage category='Prefixes'/>)}/>
+          <Route exact path = '/year:year/prefixes/:prefix' render={props => (<PracticePage category='Prefixes'/>)}/>
+          <Route exact path = '/year:year/suffixes' render={props => (<CategoriesPage category='Suffixes'/>)}/>
+          <Route exact path = '/year:year/suffixes/:suffix' render={props => (<PracticePage category='Suffixes'/>)}/>
+          <Route exact path = '/year:year/medials' render={props => (<CategoriesPage category='Medials'/>)}/>
+          <Route exact path = '/year:year/medials/:medial' render={props => (<PracticePage category='Medials'/>)}/>
+          <Route exact path = '/year:year/homophones' render={props => (<CategoriesPage category='Homophones'/>)}/>
+          <Route exact path = '/year:year/homophones/:homophone' render={props => (<PracticePage category='Homophones'/>)}/>
+          <Route exact path = '/year:year/freestyle' render={props => (<CategoriesPage category='Freestyle'/>)}/>
+          <Route exact path = '/year:year/allwords' render={props => (<CategoriesPage category='Allwords'/>)}/>  
           <Route component={NoMatchPage}/>
       </Switch>
       </React.Fragment>
