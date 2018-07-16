131689f400bf9a522e2a577b3bc6b3256b4942ac
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-16 16:44:16 +0100

    adding category as props

--- src/components/List.js
+++ src/components/List.js
@@ -3,10 +3,11 @@ import React from 'react';
 
 import WordBox from './WordBox';
 
-const List = ({ items, year, page}) => {
+const List = ({ items, year, page, category}) => {
+  console.log('category in List', category)
   return (
     <div>
-      {items.map(item => <WordBox key={item._id} item={item}  page={page} year={year}/>)}
+      {items.map(item => <WordBox key={item._id} item={item}  page={page} year={year} category={category}/>)}
     </div>
   )
 }
