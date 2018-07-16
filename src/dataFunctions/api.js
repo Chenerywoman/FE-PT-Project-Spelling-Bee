2097e578dff82b2521ee7c73f6f6c97b223ca999
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-16 17:51:59 +0100

    removing line space

--- src/dataFunctions/api.js
+++ src/dataFunctions/api.js
@@ -1,34 +1,33 @@
 const API_URL = 'http://localhost:3000/api';
 
 exports.findYears = () => {
-
     const url = `${API_URL}/years`
     return fetch(url)
-    .then(res => {
-        if (res.status === 404) throw new Error(res.statusText)
-        else return res.json()
-    })
-    .then(res =>  res)
+        .then(res => {
+            if (res.status === 404) throw new Error(res.statusText)
+            else return res.json()
+        })
+        .then(res => res)
 }
 
-exports.findCategoriesByYear  = (year) => {
+exports.findCategoriesByYear = (year) => {
     const url = `${API_URL}/categories?year=${year}`
     return fetch(url)
-    .then(res => {
-        if (res.status === 404) throw new Error(res.statusText)
-        else return res.json()
-    })
+        .then(res => {
+            if (res.status === 404) throw new Error(res.statusText)
+            else return res.json()
+        })
 }
 
 exports.findCategory = (category) => {
-category = category.toLowerCase()
+    category = category.toLowerCase()
     const url = `${API_URL}/categories/${category}`
     return fetch(url)
-    .then(res => {
-        if (res.status === 404) throw new Error(res.statusText)
-        else return res.json()
-    })
-    .then(res => res.category[0])
+        .then(res => {
+            if (res.status === 404) throw new Error(res.statusText)
+            else return res.json()
+        })
+        .then(res => res.category[0])
 }
 
 exports.findList = (category) => {
@@ -36,9 +35,9 @@ exports.findList = (category) => {
     const url = `${API_URL}/${category}`
     console.log('url', url)
     return fetch(url)
-    .then(res => {
-        if (res.status === 404) throw new Error(res.statusText)
-        else return res.json()
-    })
-    .then(res => res)
+        .then(res => {
+            if (res.status === 404) throw new Error(res.statusText)
+            else return res.json()
+        })
+        .then(res => res)
 }
