15f0c6eacf5770d9052e0b61bb64926003d025fc
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-16 20:50:20 +0100

    removing console.log

--- src/components/WordBox.js
+++ src/components/WordBox.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
-const WordBox = ({item, page, year, category}) => {
-    console.log('item in wordbox', item)
-    console.log('category in wordbox', category)
-    console.log('year in wordbox', year)
-return (
-    <React.Fragment>
-    { page === 'home' ? <Link to={`/year${item.year}`}><button>{item.year}</button></Link> 
-    :
-    page === 'year' ? <Link to={`/year${year}/${item.name}`}><button>{item.name}</button></Link>
-   : 
-   page === 'partials' ? <Link to={`/year${year}/${category}/${item.letters}`}><button>{item.letters}</button></Link>
-   : 
-   page === 'words' ? <Link to={`/year${year}/${category}`}><button>{item.word}</button></Link>
-   
-   :<div></div>
+const WordBox = ({ item, page, year, category }) => {
+
+    return (
+        <React.Fragment>
+            {page === 'home' ? <Link to={`/year${item.year}`}><button>{item.year}</button></Link>
+                :
+                page === 'year' ? <Link to={`/year${year}/${item.name}`}><button>{item.name}</button></Link>
+                    :
+                    page === 'partials' ? <Link to={`/year${year}/${category.toLowerCase()}/${item.letters}`}><button>{item.letters}</button></Link>
+                        :
+                        page === 'words' ? <div>{item.word}</div> // need inline html element or need to make wrap
+
+                            : <span> {item.word} </span> // need inline html element or need to make wrap
 
 
-    }
-    </React.Fragment>
-)
+            }
+        </React.Fragment>
+    )
 }
 
 export default WordBox;
