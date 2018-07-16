4a4a3222f964eb0b3aaa1a0561f298fee3d2b184
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-16 16:54:45 +0100

    adding partials & words pages to ternary operator

--- src/components/WordBox.js
+++ src/components/WordBox.js
@@ -2,16 +2,23 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
-const WordBox = ({item, page, year}) => {
-    console.log('item', item)
+const WordBox = ({item, page, year, category}) => {
+    console.log('item in wordbox', item)
+    console.log('category in wordbox', category)
+    console.log('year in wordbox', year)
 return (
     <React.Fragment>
     { page === 'home' ? <Link to={`/year${item.year}`}><button>{item.year}</button></Link> 
     :
     page === 'year' ? <Link to={`/year${year}/${item.name}`}><button>{item.name}</button></Link>
    : 
-   page === 'categories' ? <Link to={`/year${year}/${item.categories[0].name}/${item.letters}`}><button>{item.letters}</button></Link>
-   : <div></div>
+   page === 'partials' ? <Link to={`/year${year}/${category}/${item.letters}`}><button>{item.letters}</button></Link>
+   : 
+   page === 'words' ? <Link to={`/year${year}/${category}`}><button>{item.word}</button></Link>
+   
+   :<div></div>
+
+
     }
     </React.Fragment>
 )
