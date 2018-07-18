0deb83c5d4a6692fc6b0eef82dcc205004c426e8
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-18 14:05:08 +0100

    removing console.log

--- src/dataFunctions/helpers.js
+++ src/dataFunctions/helpers.js
@@ -1,6 +1,4 @@
 exports.notYear = (years, num) => {
-    console.log('years', typeof years[0].year, 'num', typeof num)
-
     if (years.find(year => year.year === +num) === undefined) return true
     else return false
 
