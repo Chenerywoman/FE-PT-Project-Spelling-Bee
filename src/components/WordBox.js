3d274c9f8ef42e2c26e7c8a55f363325c3bafdef
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-18 20:14:03 +0100

    adding another level to ternary to display spellings and correct word

--- src/components/WordBox.js
+++ src/components/WordBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
-const WordBox = ({ item, page, year, category }) => {
+const WordBox = ({ item, page, year, category, style }) => {
 
     return (
         <React.Fragment>
@@ -13,8 +13,10 @@ const WordBox = ({ item, page, year, category }) => {
                     page === 'partials' ? <Link to={`/year${year}/${category.toLowerCase()}/${item.letters}`}><button>{item.letters}</button></Link>
                         :
                         page === 'words' ? <div>{item.word}</div> // need inline html element or need to make wrap
-
-                            : <span> {item.word} </span> // need inline html element or need to make wrap
+                            : 
+                            page === 'practice' ? <span style={style} > {item.word} </span> 
+                            :
+                            <p style={style} >{item.spelling} {item.correctSpelling}</p>
 
 
             }
