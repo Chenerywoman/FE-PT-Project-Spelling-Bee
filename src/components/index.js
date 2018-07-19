4113c8fc70c91b3111f7ecb021b36a8160697c0f
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-19 17:01:59 +0100

    adding SpellingBox.js to index

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import App from './App';
 import List from './List';
 import ContentBox from './ContentBox';
 import WordBox from './WordBox';
+import SpellingBox from './SpellingBox';
 
-export {App, List, ContentBox, WordBox}
+export {App, List, ContentBox, WordBox, SpellingBox}
