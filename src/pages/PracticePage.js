a50ddec78ba2ae78e9f85118bf2d3d5522de6e3d
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-19 09:38:38 +0100

    moving onvoiceschange event handler to onMount to prevent infinite loop blocking loading

--- src/pages/PracticePage.js
+++ src/pages/PracticePage.js
@@ -32,21 +32,14 @@ class PracticePage extends Component {
 
     handlePracticeClick = () => {
         this.setState({ showForm: true })
+        // window.speechSynthesis.onvoiceschanged = this.voicesLoaded
     }
 
-    handleSpellSlowClick = () => {
+    handleSpellClick = () => {
         const voices = window.speechSynthesis.getVoices()
         let utterance = new SpeechSynthesisUtterance(this.state.words[this.state.wordsIndex].word);
         utterance.voice = voices.find(voice => voice.name === 'Fiona');
         utterance.rate = 0.5;
-        window.speechSynthesis.speak(utterance);
-    }
-
-    handleSpellFastClick = () => {
-        const voices = window.speechSynthesis.getVoices()
-        let utterance = new SpeechSynthesisUtterance(this.state.words[this.state.wordsIndex].word);
-        utterance.voice = voices.find(voice => voice.name === 'Fiona');
-        utterance.rate = 1;
         window.speechSynthesis.speak(utterance);
     }
 
@@ -67,34 +60,33 @@ class PracticePage extends Component {
 
     componentDidMount() {
         this.getWords(this.props.category, this.props.match.params.letters)
+        .then(() => window.speechSynthesis.onvoiceschanged = this.voicesLoaded)
+
     }
 
     render() {
-        console.log('this.state.spellings', this.state.spellings)
-        window.speechSynthesis.onvoiceschanged = this.voicesLoaded
-        return this.state.voices && (
+            return (
             <React.Fragment>
                 {!/^[1-6]$/.test(this.props.match.params.year) ? < Redirect to='/404' />
                     : this.state.loading ? <p>loading...</p>
                         :
                         <React.Fragment>
-                            <header><h1>Time to Practice!</h1></header>
+                            <header><h1>Time to Practise!</h1></header>
                             <img src={BeeLogoLarge} id="BeeLogoLarge" className="bee-logo" alt="BeeLogoLarge" />
                             {notYear(this.state.words[0].years, this.props.match.params.year) ?
                                 <p>No {this.state.label} {this.props.match.params.letters} for year {this.props.match.params.year}.</p>
                                 :
                                 <React.Fragment>
-                                    <ContentBox className="content" description={`${this.state.label}: ${this.props.match.params.letters}`} />
-                                    <button onClick={this.handleSpellSlowClick} style={{ display: this.state.showForm ? 'inline' : 'none' }}>Play word slowly</button>
-                                    <button onClick={this.handleSpellFastClick} style={{ display: this.state.showForm ? 'inline' : 'none' }}>Play word fast</button>
+                                    <ContentBox className="content" description={`${this.state.label}: ${this.props.match.params.letters} remaining words: ${this.state.words.length - this.state.wordsIndex}`} />
+                                    <button onClick={this.handleSpellClick} style={{ display: this.state.showForm ? 'inline' : 'none' }} disabled={this.state.wordsIndex === this.state.words.length  ? true : false}>Play word</button>
                                     <List className="list" items={this.state.words} page='practice' year={this.props.match.params.year} category={this.props.category}
                                         style={{ display: this.state.showForm ? 'none' : 'inline' }} />
-                                    <button onClick={this.handlePracticeClick} style={{ display: this.state.showForm ? 'none' : 'inline' }}> Let's Practice</button>
+                                    <button onClick={this.handlePracticeClick} style={{ display: this.state.showForm ? 'none' : 'inline' }}> Let's Practise</button>
                                     <form onSubmit={this.handleSubmit} style={{ display: this.state.showForm ? 'inline' : 'none' }} >
                                         <label>
                                             Your spelling: <input type="text" value={this.state.spelling} onChange={this.handleChange} />
                                         </label>
-                                        <input type="submit" value="Check your spelling" />
+                                        <input type="submit" value="Check your spelling" disabled={this.state.wordsIndex === this.state.words.length  ? true : false}/>
                                     </form>
                                     <List className="list" items={this.state.spellings} page='answers' year={this.props.match.params.year} category={this.props.category}
                                         style={{ display: this.state.showForm ? 'block' : 'none' }} />
