a224bfbba7f8c49d1f4cf03dc0ab1ca56070ffb7
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-19 17:39:32 +0100

    adding updateScore function to update total in state & adding to componentDidMount & componentDidUpdate

--- src/components/SpellingBox.js
+++ src/components/SpellingBox.js
@@ -1,34 +1,63 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styling/components/SpellingBox.css';
 
 // spellings, correctSpellings, year, category, style
 
 class SpellingBox extends Component {
 
+  state = {
 
-checkWord = (word) => {
+    total: 0
+
+  }
+
+  checkWord = (word) => {
     return word.every(char => char.correct)
-}
+  }
 
-makeWord = (word) => {
+  makeWord = (word) => {
 
-  return word.map(({letter, correct}) => {
-      return <span className={correct ? 'green' : 'red'} >{letter}</span> 
+    return word.map(({ letter, correct }) => {
+      return <span className={correct ? 'green' : 'red'} >{letter}</span>
     })
 
-}
+  }
 
-render () {
+  checkWord = (word) => {
+    return word.every(char => char.correct)
+  }
 
-  return this.props.spellings.map((word, ind) => {
-    return (   <React.Fragment>
-       <p> <span>{this.makeWord(word)}</span> <span>{this.checkWord(word) ? '' : this.props.correctSpellings[ind].word}</span> </p>
-        </React.Fragment>
-     )  
-})
+  updateScore = (word) => {
+    if (word.every(char => char.correct)) {
+      const newTotal = this.state.total + 1
+      this.setState({total: newTotal})
+    }
+  }
 
+  componentDidMount () {
     
-}
+    this.props.spellings.map(word => this.updateScore(word))
+
+  }
+
+  componentDidUpdate (prevProps) {
+    
+    if (prevProps.spellings !== this.props.spellings) {
+    this.props.spellings.map(word => this.updateScore(word))
+    }
+
+  }
+
+  render() {
+
+    return <React.Fragment> 
+      {this.props.spellings.map((word, ind) => {
+        return <p> <span>{this.makeWord(word)}</span> <span>{this.checkWord(word) ? '' : this.props.correctSpellings[ind].word}</span> </p>
+      })
+      }
+      <p> {`Score: ${this.state.total} / ${this.props.correctSpellings.length}`}</p>
+      </React.Fragment>
+  }
 
 }
 
