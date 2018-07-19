8fdb72f2c8a6a89d418148add568aeba3fc7baae
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-19 22:19:03 +0100

    adding resized png to index

--- src/logos/index.js
+++ src/logos/index.js
@@ -3,5 +3,6 @@ import BeeLogoLarge from '../logos/BeeLogoLarge.png';
 import BeeLogo500px from '../logos/BeeLogo500px.png';
 import BeeLogo250px from '../logos/BeeLogo250px.png';
 import BeeLogo100px from '../logos/BeeLogo100px.png';
+import BeeLogo100pxtoresize from '../logos/BeeLogo100pxtoresize.png'
 
-export {BeeLogoLargeBordered, BeeLogoLarge, BeeLogo500px, BeeLogo250px, BeeLogo100px}
+export {BeeLogoLargeBordered, BeeLogoLarge, BeeLogo500px, BeeLogo250px, BeeLogo100px, BeeLogo100pxtoresize}
