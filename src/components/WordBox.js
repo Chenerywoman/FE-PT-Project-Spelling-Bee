723b82df031ab557bb0a68ea315866d496a72284
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-19 22:20:48 +0100

    adding ids to enable individual button styling

--- src/components/WordBox.js
+++ src/components/WordBox.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styling/components/WordBox.css';
 // import PropTypes from 'prop-types';
 
 const WordBox = ({ item, page, year, category, style }) => {
 
     return (
         <React.Fragment>
-            {page === 'home' ? <Link to={`/year${item.year}`}><button>{item.year}</button></Link>
+            {page === 'home' ? <Link to={`/year${item.year}`}><button id={`year${item.year}`} >{`Year ${item.year}`}</button></Link>
                 :
-                page === 'year' ? <Link to={`/year${year}/${item.name}`}><button>{item.name}</button></Link>
+                page === 'year' ? <Link to={`/year${year}/${item.name}`}><button id={item.name}>{item.name}</button></Link>
                     :
                     page === 'partials' ? <Link to={`/year${year}/${category.toLowerCase()}/${item.letters}`}><button>{item.letters}</button></Link>
                         :
