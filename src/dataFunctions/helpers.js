2f15a1ba2310741df2004e7901a20c963ecc5f7f
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-19 09:56:25 +0100

    adding chunckarray function

--- src/dataFunctions/helpers.js
+++ src/dataFunctions/helpers.js
@@ -1,5 +1,19 @@
-exports.notYear = (years, num) => {
+
+export const notYear = (years, num) => {
     if (years.find(year => year.year === +num) === undefined) return true
     else return false
+}
 
+
+export const chunkArray = (array, chunk) => {
+
+    const newArray = [...array]
+
+    let results = [];
+    
+    while (newArray.length) {
+        results.push(newArray.splice(0, chunk));
+    }
+    
+    return results;
 }
