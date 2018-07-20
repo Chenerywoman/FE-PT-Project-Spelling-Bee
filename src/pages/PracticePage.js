b1d56edc3167508cada7ab0d475bdf7cc2d625da
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-20 22:17:31 +0100

    refactoring with ternary into showform & not show form & adding ids for css

--- src/pages/PracticePage.js
+++ src/pages/PracticePage.js
@@ -38,7 +38,7 @@ class PracticePage extends Component {
 
     componentDidMount() {
         this.getWords(this.props.category, this.props.match.params.letters)
-        .then(() => window.speechSynthesis.onvoiceschanged = this.voicesLoaded)
+            .then(() => window.speechSynthesis.onvoiceschanged = this.voicesLoaded)
 
     }
 
@@ -64,74 +64,93 @@ class PracticePage extends Component {
     handleSubmit = (event) => {
         event.preventDefault();
         const result = checkSpelling(this.state.spelling, this.state.words[this.state.arrayIndex][this.state.wordsIndex].word)
-        this.setState({ spellings: [...this.state.spellings, result], wordsIndex: this.state.wordsIndex + 1, spelling: '' }) 
+        this.setState({ spellings: [...this.state.spellings, result], wordsIndex: this.state.wordsIndex + 1, spelling: '' })
     }
 
     handlePracticeAgain = () => {
-        this.setState({wordsIndex: 0, spellings: [] })
+        this.setState({ wordsIndex: 0, spellings: [] })
     }
 
     handleNextWords = () => {
         const newArrayIndex = this.state.arrayIndex + 1
-        this.setState({wordsIndex: 0, arrayIndex: newArrayIndex, spellings: [] })
+        this.setState({ wordsIndex: 0, arrayIndex: newArrayIndex, spellings: [] })
     }
 
     handlePreviousWords = () => {
         const newArrayIndex = this.state.arrayIndex - 1
-        this.setState({wordsIndex: 0, arrayIndex: newArrayIndex, spellings: [] })
+        this.setState({ wordsIndex: 0, arrayIndex: newArrayIndex, spellings: [] })
 
     }
 
     render() {
         return (
             <React.Fragment>
-                {!/^[1-6]$/.test(this.props.match.params.year) ? < Redirect to='/404' />
-                    : this.state.loading ? <p>loading...</p>
-                        :
-                        <div id='practicepage_container'>
-                            <header><NavBar page='practice' year={this.props.match.params.year} category={this.props.category} username={this.props.username} />
-                                <h1>Practise <span id='practiseHighlight'>{`${this.state.label}:${this.props.match.params.letters}`} </span></h1></header>
-                            <Link className='link' to='/'><img src={BeeLogo250px} id="BeeLogo250px" className="bee-logo" alt="BeeLogo250px" /></Link>
-                            {notYear(this.state.years, this.props.match.params.year) ?
-                                <p>No {this.state.label} {this.props.match.params.letters} for year {this.props.match.params.year}.</p>
-                                :
-                                <React.Fragment>
-                                    { this.state.showForm ? <div></div> : 
-                                    <ContentBox page='practicepage' 
-                                    description={`${this.props.username}, practise these words with the ${this.state.label} '${this.props.match.params.letters}' 5 words at a time.`} 
-                                    /> }
-                                    
-                                    <button className='practisebutton' onClick={this.handlePracticeClick} style={{ display: this.state.showForm ? 'none' : 'inline' }}> Let's Go!</button>
+                {
 
-                                    <List className="list" items={this.state.words} page='practice' year={this.props.match.params.year} category={this.props.category}
-                                        style={{ display: this.state.showForm ? 'none' : 'inline' }} />
-                                    
-                                    { this.state.showForm ? <ContentBox page='practicepage' 
-                                    description={`${this.props.username}, click the buttons to practise your spellings 5 words at a time.`}/> 
-                                    : <div></div>  } 
+                    !/^[1-6]$/.test(this.props.match.params.year) ? < Redirect to='/404' />
 
-                                   <p> <button className='practisebutton' onClick={this.handleSpellClick} style={{ display: this.state.showForm ? 'inline' : 'none' }} disabled={this.state.wordsIndex === this.state.words[this.state.arrayIndex].length ? true : false}>Play word</button></p>    
-                                    
-                                    <form onSubmit={this.handleSubmit} style={{ display: this.state.showForm ? 'inline' : 'none' }} >
-                                        <input type="text" placeholder="spell here" value={this.state.spelling} onChange={this.handleChange} />
-                                        <input type="submit" value="Check spelling" disabled={this.state.wordsIndex === this.state.words[this.state.arrayIndex].length ? true : false} />
-                                    </form>
-                            { this.state.spellings.length ? <SpellingBox spellings={this.state.spellings} correctSpellings={this.state.words[this.state.arrayIndex]} year={this.props.match.params.year} category={this.props.category} style={{ display: this.state.showForm ? 'block' : 'none' }} /> : <div></div> }
-                                    
-                                    <button onClick={this.handleNextWords} style={{ display: this.state.showForm && this.state.wordsIndex === this.state.words[this.state.arrayIndex].length && this.state.arrayIndex < this.state.words.length -1 ? 'inline' : 'none' }}
-                                    disabled={this.state.words.length -1 === this.state.arrayIndex}> Next words</button>
-                                    
-                                    <button onClick={this.handlePreviousWords} style={{ display: this.state.showForm && this.state.wordsIndex === this.state.words[this.state.arrayIndex].length && this.state.arrayIndex > 0 ? 'inline' : 'none' }}
-                                    disabled={this.state.arrayIndex === 0}> Previous words</button>
-                                    
-                                    <button onClick={this.handlePracticeAgain} style={{ display: this.state.showForm && this.state.wordsIndex === this.state.words[this.state.arrayIndex].length ? 'inline' : 'none' }}> Practise again</button>
-                                </React.Fragment>
-                            }
-                        </div>
+                        : this.state.loading ? <p>loading...</p>
+
+                            :
+
+                            <div id='practicepage_container'>
+                                <header><NavBar page='practice' year={this.props.match.params.year} category={this.props.category} username={this.props.username} />
+                                    <h1>Practise <span id='practiseHighlight'>{`${this.state.label}:${this.props.match.params.letters}`} </span></h1></header>
+                                {
+                                    notYear(this.state.years, this.props.match.params.year) ?
+                                        <p>No {this.state.label} {this.props.match.params.letters} for year {this.props.match.params.year}.</p>
+                                        :
+                                        <React.Fragment>
+
+                                            {
+                                                !this.state.showForm ?
+
+                                                    <React.Fragment>
+                                                        <Link className='link' to='/'><img src={BeeLogo250px} id="BeeLogo250px" className="bee-logo" alt="BeeLogo250px" /></Link>
+                                                        <ContentBox page='practicepage' description={`${this.props.username}, practise these words with the ${this.state.label} '${this.props.match.params.letters}' 5 words at a time.`} />
+                                                        <button className='practisebutton' onClick={this.handlePracticeClick} > Let's Go!</button>
+                                                        <List className="list" items={this.state.words} page='practice' year={this.props.match.params.year} category={this.props.category} />
+
+                                                    </React.Fragment>
+
+                                                    :
+
+                                                    <React.Fragment>
+                                                        <ContentBox page='practicepage' description={`${this.props.username}, click the buttons to practise your spellings 5 words at a time.`} />
+                                                        <p> <button className='practisebutton' onClick={this.handleSpellClick} disabled={this.state.wordsIndex === this.state.words[this.state.arrayIndex].length ? true : false}>Play word</button></p>
+
+                                                        <form onSubmit={this.handleSubmit}  >
+                                                            <input id='inputSpelling' type="text" placeholder="spell here" value={this.state.spelling} onChange={this.handleChange} />
+                                                            <input id='checkSpelling' type="submit" value="Check spelling" disabled={!this.state.spelling || this.state.wordsIndex === this.state.words[this.state.arrayIndex].length ? true : false} />
+                                                        </form>
+
+                                                        {this.state.spellings.length ?
+                                                            <SpellingBox spellings={this.state.spellings} correctSpellings={this.state.words[this.state.arrayIndex]} year={this.props.match.params.year} category={this.props.category} />
+                                                            : <div></div>
+                                                        }
+
+                                                        <button id='nextWords' onClick={this.handleNextWords} style={{ display: this.state.wordsIndex === this.state.words[this.state.arrayIndex].length && this.state.arrayIndex < this.state.words.length - 1 ? 'inline' : 'none' }}
+                                                            disabled={this.state.words.length - 1 === this.state.arrayIndex}> Next words</button>
+
+                                                        <button id='previousWords' onClick={this.handlePreviousWords} style={{ display: this.state.wordsIndex === this.state.words[this.state.arrayIndex].length && this.state.arrayIndex > 0 ? 'inline' : 'none' }}
+                                                            disabled={this.state.arrayIndex === 0}> Previous words</button>
+
+                                                        <button id='practiceAgain' onClick={this.handlePracticeAgain} style={{ display: this.state.wordsIndex === this.state.words[this.state.arrayIndex].length ? 'inline' : 'none' }}> Practise again</button>
+                                                    </React.Fragment>
+
+
+
+                                            }
+
+                                        </React.Fragment>
+                                }
+                            </div>
                 }
             </React.Fragment>
         );
+
     }
+
 }
 
 export default PracticePage;
