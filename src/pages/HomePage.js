c8f95e2034bb06c3895a90567e5c6f8a6012622c
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-20 10:54:28 +0100

    adding changeLogin function to toggle between user greeting & userlogin

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BeeLogo100pxtoresize, BeeLogo250px } from '../logos';
 import { ContentBox, List } from '../components';
-import {findYears} from '../dataFunctions/api'
+import { findYears } from '../dataFunctions/api'
 import '../styling/pages/HomePage.css';
 
 class HomePage extends Component {
@@ -10,61 +10,74 @@ class HomePage extends Component {
     years: [],
     loading: true,
     value: '',
-    userlogged: false, 
-        username: ''
+    userlogged: false,
+    username: ''
   }
 
   handleChange = (event) => {
     this.setState({ value: event.target.value });
-}
+  }
 
-handleSubmit = event => {
+  handleSubmit = event => {
     event.preventDefault();
     this.props.logUser(this.state.value)
-}
+    this.setState({value: '', userlogged: true})
+  }
 
-  componentDidMount () {
+  handleLoginChange = event => {
+    event.preventDefault();
+    this.setState({userlogged: false})
+  }
+
+  componentDidMount() {
     return findYears()
-    .then(({years}) => {
-      this.setState({years, loading: false})
-      if (localStorage.username) {
-        this.setState({ userlogged: true })
-  }
-    })
-    .catch(error => {
-      this.props.history.push('/404');
-    });
+      .then(({ years }) => {
+        this.setState({ years, loading: false })
+        if (localStorage.username) {
+          this.setState({ userlogged: true })
+        }
+      })
+      .catch(error => {
+        this.props.history.push('/404');
+      });
   }
 
-  componentDidUpdate (prevProps) {
+  componentDidUpdate(prevProps) {
     if (this.props.username !== prevProps.username) {
       this.setState({ userlogged: true })
-  }
+    }
 
   }
 
   render() {
     return (
       <React.Fragment>
-      <header>
-      <h1>Welcome to Spelling Bee!</h1></header>
-      <img src={BeeLogo250px} id="BeeLogo250px" className="bee-logo" alt="BeeLogoLarge" />
-   {
-      this.state.loading ?   <p> Loading...</p>
-    :
-    <React.Fragment>
-       <form onSubmit={this.handleSubmit} id='loginform'>
-       <label>
-        What is your name? <input id='inputName' type="text" value={this.state.value} onChange={this.handleChange} />
-      </label>
-      <input id={BeeLogo100pxtoresize} type="image" name="submit" src={BeeLogo100pxtoresize} border="0" alt="Submit" />
-       </form>
-    <ContentBox description='Choose your school year:' page='homepage' />
-    <List items={this.state.years} page='home'/>
-    </React.Fragment>
-   }
-   </React.Fragment>
-      )
+    
+          <h1>Welcome to Spelling Bee</h1>
+        <img src={BeeLogo250px} id="BeeLogo250px" className="bee-logo" alt="BeeLogoLarge" />
+        {
+          this.state.loading ? <p> Loading...</p>
+            :
+            <React.Fragment>
+            {!this.state.userlogged ? 
+            <React.Fragment>
+              <form onSubmit={this.handleSubmit} id='loginform'>
+                <input id='addName' placeholder='type your name here' type="text" value={this.state.value} onChange={this.handleChange} />
+                <span id={BeeLogo100pxtoresize}><input type="image" name="submit" src={BeeLogo100pxtoresize} alt="Submit" /></span>
+              </form>
+              </React.Fragment>
+              :
+              <React.Fragment>
+               <h2 id='greeting' >{`Hi ${this.props.name}!`}</h2> 
+               <button id='changeLogin' onClick={this.handleLoginChange} >change name</button> 
+              <ContentBox description='Choose your school year:' page='homepage' />
+              <List items={this.state.years} page='home' />
+            </React.Fragment>
+            }
+            </React.Fragment>
+        }
+      </React.Fragment>
+    )
 
   }
 
