859d116be3f29ee89770f67a1ad1728ee4d41f20
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-21 11:43:29 +0100

    refactoring to have separate navbars for years & categories

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,31 +3,24 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../styling/components/NavBar.css'
 
-class Navbar extends Component {
+class NavBar extends Component {
 
     render(){
     return (<nav>
-        <ul id='list' className='flex-container-navbar'>
-            {/* <li><Link className='link' to='/'><button id='home'>Home</button></Link></li>  */}
+        <ul className='flex-container-navbar'>
             <li ><Link className={this.props.year === '1' ? 'link-active' : 'link'}  to='/year1' ><button id='year1link'>Year 1</button></Link></li>
             <li ><Link className={this.props.year === '2' ? 'link-active' : 'link'}  to='/year2' ><button id='year2link'>Year 2</button></Link></li>
             <li ><Link className={this.props.year === '3' ? 'link-active' : 'link'}  to='/year3' ><button id='year3link'>Year 3</button></Link></li>
             <li ><Link className={this.props.year === '4' ? 'link-active' : 'link'}  to='/year4' ><button id='year4link'>Year 4</button></Link></li>
             <li ><Link className={this.props.year === '5' ? 'link-active' : 'link'}  to='/year5' ><button id='year5link'>Year 5</button></Link></li>
             <li ><Link className={this.props.year === '6' ? 'link-active' : 'link'}  to='/year6' ><button id='year6link'>Year 6</button></Link></li>
-          {/* {this.props.category ?  <li ><Link to={`/year${this.props.year}/${this.props.category.toLowerCase()}`} ><button id='categoriesLink'>{this.props.category.toLowerCase()}</button></Link></li> : <div></div>} */}
-            {/* <li ><Link  className={this.props.category === 'Suffixes' ? 'link-active' : 'link'} to={`/year${this.props.year}/suffixes`} ><button id='suffixesLink'>suffixes</button></Link></li>
-            <li ><Link  className={this.props.category === 'Medials' ? 'link-active' : 'link'} to={`/year${this.props.year}/medials`} ><button id='medialsLink'>medials</button></Link></li>
-            <li ><Link  className={this.props.category === 'Homophones' ? 'link-active' : 'link'} to={`/year${this.props.year}/homophones`} ><button id='homophonesLink'>homophones</button></Link></li>
-            <li ><Link  className={this.props.category === 'Freestyle' ? 'link-active' : 'link'} to={`/year${this.props.year}/freestyle`} ><button id='freestyleLink'>freestyle</button></Link></li>
-            <li ><Link  className={this.props.category === 'Words' ? 'link-active' : 'link'} to={`/year${this.props.year}/words`} ><button id='wordsLink'>words</button></Link></li> */}
         </ul>
-    </nav>
+        </nav>
     )
     }
 }
 
-Navbar.propTypes = {
+NavBar.propTypes = {
 
     username: PropTypes.string.isRequired,
     page: PropTypes.string.isRequired,
@@ -35,4 +28,4 @@ Navbar.propTypes = {
 
 }
 
-export default Navbar;
+export default NavBar;
---
+++ src/components/NavBarCategories.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import '../styling/components/NavBarCategories.css'
+
+class NavBarCategories extends Component {
+
+    render(){
+    return (<nav>
+         <ul className='flex-container-navbar-categories'>
+            <li ><Link  className={this.props.category === 'Prefixes' ? 'category-link-active' : 'category-link'} to={`/year${this.props.year}/prefixes`} ><span id='prefixesLink'>Prefixes</span></Link></li>
+            <li ><Link  className={this.props.category === 'Suffixes' ? 'category-link-active' : 'category-link'} to={`/year${this.props.year}/suffixes`} ><span id='suffixesLink'>Suffixes</span></Link></li>
+            <li ><Link  className={this.props.category === 'Medials' ? 'category-link-active' : 'category-link'} to={`/year${this.props.year}/medials`} ><span id='medialsLink'>Medials</span></Link></li>
+            <li ><Link  className={this.props.category === 'Homophones' ? 'category-link-active' : 'category-link'} to={`/year${this.props.year}/homophones`} ><span id='homophonesLink'>Homophones</span></Link></li>
+            <li ><Link  className={this.props.category === 'Freestyle' ? 'category-link-active' : 'category-link'} to={`/year${this.props.year}/freestyle`} ><span id='freestyleLink'>Freestyle</span></Link></li>
+            <li ><Link  className={this.props.category === 'Words' ? 'category-link-active' : 'category-link'} to={`/year${this.props.year}/words`} ><span id='wordsLink'>Words</span></Link></li> 
+            </ul> 
+    </nav>
+    )
+    }
+}
+
+NavBarCategories.propTypes = {
+
+    username: PropTypes.string.isRequired,
+    page: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+
+}
+
+export default NavBarCategories;
