1f9ad2a0b8aec9f0304f9e7a278846d1bb56ce5f
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-24 10:35:13 +0100

    adding additional logic to updateScore to check for s length

--- src/components/SpellingBox.js
+++ src/components/SpellingBox.js
@@ -10,7 +10,7 @@ class SpellingBox extends Component {
 
   checkWord = (spelling, word) => {
     if (spelling.every(char => char.correct) && spelling.length === word.word.length)
-    return true;
+      return true;
   }
 
   makeWord = (word) => {
@@ -21,46 +21,49 @@ class SpellingBox extends Component {
 
   }
 
-  updateScore = (word) => {
+  updateScore = (spelling, word) => {
     const newTotal = this.state.total + 1
-    return  word.every(char => char.correct) ? this.setState({total: newTotal}) : this.setState({total: this.state.total})
+    if (spelling.every(char => char.correct) && spelling.length === word.word.length) {
+      this.setState({ total: newTotal })
     }
+    else this.setState({ total: this.state.total })
+  }
 
-  componentDidMount () {
-    
-    this.props.spellings.map(word => this.updateScore(word))
+  componentDidMount() {
+
+    this.props.spellings.map((spelling, ind) => this.updateScore(spelling, this.props.correctSpellings[ind]))
 
   }
 
-  componentDidUpdate (prevProps) {
-    
+  componentDidUpdate(prevProps) {
+
     if (prevProps.spellings !== this.props.spellings) {
-    this.props.spellings.map(word => this.updateScore(word))
+      this.props.spellings.map((spelling, ind) => this.updateScore(spelling, this.props.correctSpellings[ind]))
     }
 
   }
 
   render() {
-    return <React.Fragment> 
+    return <React.Fragment>
       <table id='spelling-table'>
-      <tbody>
-      <tr>
-    <th className='table-heading' >Your Spelling</th>
-    <th className='table-heading'>Correct Spelling</th> 
-    </tr> 
-      {this.props.spellings.map((word, ind) => 
-        <tr key={ind}>
-        <td>{this.makeWord(word)}</td> 
-        <td className='green'>{this.checkWord(word, this.props.correctSpellings[ind]) ? <div>&#x2714;</div> : this.props.correctSpellings[ind].word}</td>
-      </tr>
-      )
-      }
-      </tbody>
+        <tbody>
+          <tr>
+            <th className='table-heading' >Your Spelling</th>
+            <th className='table-heading'>Correct Spelling</th>
+          </tr>
+          {this.props.spellings.map((spelling, ind) =>
+            <tr key={ind}>
+              <td>{this.makeWord(spelling)}</td>
+              <td className='green'>{this.checkWord(spelling, this.props.correctSpellings[ind]) ? <div>&#x2714;</div> : this.props.correctSpellings[ind].word}</td>
+            </tr>
+          )
+          }
+        </tbody>
       </table>
-    { this.props.spellings.length === this.props.correctSpellings.length ? 
-    <p id='score'> {`Score: ${this.state.total} / ${this.props.correctSpellings.length}`}</p> 
-    : <div></div> }
-      </React.Fragment>
+      {this.props.spellings.length === this.props.correctSpellings.length ?
+        <p id='score'> {`Score: ${this.state.total} / ${this.props.correctSpellings.length}`}</p>
+        : <div></div>}
+    </React.Fragment>
   }
 
 }
